699f3882ee919c71202ff0c2415a4dc9fb7931b5
Erkam Seker <serkamseker@gmail.com>
2017-03-25 00:00:48 +0300

    bug fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
    res.send('bigezdir');
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8082, function () {
    var host = server.address().address;
    var port = server.address().port;
    
