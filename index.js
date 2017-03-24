e4be888559ee7fc4b679cb004a57bcff759d7ffd
Erkam Seker <serkamseker@gmail.com>
2017-03-25 02:02:10 +0300

    dummy data 2

--- index.js
+++ index.js
@@ -23,111 +23,30 @@ var server = app.listen(process.env.PORT || 8082, function () {
 });
 
 var userObj = new userDb({	
-  		first_name: "Melih",
-  		last_name: "Mutlu",
-  		age: 33,
-  		bio: "Boğaziçi Üniversitesi",
-  		pic_url: "http://i.hizliresim.com/7qqm1W.jpg",
-  		hangout_place: "ChIJuQfSbQbQyhQRo686317fLx4",
-  		hangout_date: "26.3.2017",
-  		hangout_hour: "aksam",
-  		id: "2" });
-
-	userObj.save(function(err) {
-  		if (err) console.log(err);
-  		console.log(userObj.first_name + ' saved successfully!');
-	});
-
-var userObj = new userDb({	
-  		first_name: "Erkam",
-  		last_name: "Şeker",
-  		age: 21,
-  		bio: "Takılma amaçlı..",
-  		pic_url: "http://i.hizliresim.com/7qqm1W.jpg",
-  		hangout_place: "ChIJuQfSbQbQyhQRo686317fLx4",
-  		hangout_date: "26.3.2017",
-  		hangout_hour: "aksam",
-  		id: "3" });
-
-	userObj.save(function(err) {
-  		if (err) console.log(err);
-  		console.log(userObj.first_name + ' saved successfully!');
-	});
-var userObj = new userDb({	
-  		first_name: "Tuğçe",
-  		last_name: "Hünerli",
-  		age: 19,
-  		bio: "Öğrenci",
-  		pic_url: "http://i.hizliresim.com/7qqm1W.jpg",
-  		hangout_place: "ChIJuQfSbQbQyhQRo686317fLx4",
-  		hangout_date: "26.3.2017",
-  		hangout_hour: "aksam",
-  		id: "4" });
-
-	userObj.save(function(err) {
-  		if (err) console.log(err);
-  		console.log(userObj.first_name + ' saved successfully!');
-	});
-var userObj = new userDb({	
   		first_name: "Burcu",
   		last_name: "Vuslat",
   		age: 23,
   		bio: "Sinefil ve dizisever...",
-  		pic_url: "http://i.hizliresim.com/7qqm1W.jpg",
+  		pic_url: "http://i.hizliresim.com/V00pmy.jpg",
   		hangout_place: "ChIJuQfSbQbQyhQRo686317fLx4",
   		hangout_date: "26.3.2017",
   		hangout_hour: "aksam",
   		id: "5" });
-
+userObj.save(function(err) {
+  		if (err) console.log(err);
+  		console.log(userObj.first_name + ' saved successfully!');
+	});
 var userObj = new userDb({	
   		first_name: "Betül",
   		last_name: "Aras",
   		age: 21,
   		bio: "Bilgi University",
-  		pic_url: "http://i.hizliresim.com/7qqm1W.jpg",
+  		pic_url: "http://i.hizliresim.com/V00pby.jpg",
   		hangout_place: "ChIJuQfSbQbQyhQRo686317fLx4",
   		hangout_date: "26.3.2017",
   		hangout_hour: "aksam",
   		id: "6" });
-
-var userObj = new userDb({	
-  		first_name: "Elif",
-  		last_name: "Gezgin",
-  		age: 20,
-  		bio: "#museum #art #hiking",
-  		pic_url: "http://i.hizliresim.com/7qqm1W.jpg",
-  		hangout_place: "ChIJuQfSbQbQyhQRo686317fLx4",
-  		hangout_date: "26.3.2017",
-  		hangout_hour: "aksam",
-  		id: "7" });
-	userObj.save(function(err) {
+userObj.save(function(err) {
   		if (err) console.log(err);
   		console.log(userObj.first_name + ' saved successfully!');
-	});
-var hangoutObj = new hangoutDb({	
-  		place: "ChIJuQfSbQbQyhQRo686317fLx4",
-  		date: "26.3.2017",
- 		hour: "aksam" });
-
-	hangoutObj.save(function(err) {
-  		if (err) console.log(err);
-  		console.log(hangoutObj.place + ' saved successfully!');
-	});
-var hangoutObj = new hangoutDb({	
-  		place: "ChIJJeArFfzJyhQRBO0aNppJLR4",
-  		date: "26.3.2017",
- 		hour: "aksam" });
-
-	hangoutObj.save(function(err) {
-  		if (err) console.log(err);
-  		console.log(hangoutObj.place + ' saved successfully!');
-	});
-var hangoutObj = new hangoutDb({	
-  		place: "ChIJuSZM1__JyhQRhwXKF-O5Jek",
-  		date: "26.3.2017",
- 		hour: "aksam" });
-
-	hangoutObj.save(function(err) {
-  		if (err) console.log(err);
-  		console.log(hangoutObj.place + ' saved successfully!');
 	});
