817b04cdbd285c1ca198865b5c3e46835a72ca4a
Erkam Seker <serkamseker@gmail.com>
2017-03-25 02:14:53 +0300

    bugg

--- dbHelper.js
+++ dbHelper.js
@@ -3,7 +3,7 @@ var dateDb = require("./models/Hangout");
 var mongoose = require('mongoose');
 
 var getUsers = function(place, date, hour, callback){
-	User.find({ hangout_place: place, hangout_date: date, hangout_hour: hour }, function(err, users){
+	userDb.find({ hangout_place: place, hangout_date: date, hangout_hour: hour }, function(err, users){
 		if(err){
 			console.log(err);
 			throw err;
