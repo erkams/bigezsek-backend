82e2d9ab845cb17630ef3b0dc824716086b8481a
Erkam Seker <serkamseker@gmail.com>
2017-03-24 23:39:24 +0300

    databases are configured.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var User = require("../models/User");
+var User = require("../models/Date");
+var mongoose = require('mongoose');
 var app = express();
 
-app.get('/test', function (request, response) {
-    console.log("get request");
-    response.sendFile("I got some json for you ;)");
+app.get('/', function (req, res) {
+   res.send('bigezdir');
 });
 
-var server = app.listen(process.env.PORT || 8082, function () {
-
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("Example app listening at http://%s:%s", host, port);
-
+var server = app.listen(8081, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   
+   console.log("http://%s:%s listening..", host, port);
 });
--- models/Date.js
+++ models/Date.js
@@ -2,18 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var con = mongoose.createConnection('mongodb://bigezsek:1123581321@ds141490.mlab.com:41490/heroku_gj10ngvg');
 
-var userSchema = new Schema({
-  first_name: String,
-  last_name: String,
-  age: Number,
-  bio: String,
-  pic_url: String,
-  id: { type: String, required: true, unique: true },
+var dateSchema = new Schema({
+  id: { type: String, required: true },
+  date: { type: String, required: true },
+  hour: { type: String, required: true },
+  users: Array,
   created_at: Date,
   updated_at: Date
 });
 
-userSchema.pre('save', function(next) {
+dateSchema.pre('save', function(next) {
   var currentDate = new Date();
   
   this.updated_at = currentDate;
@@ -24,4 +22,4 @@ userSchema.pre('save', function(next) {
   next();
 });
 
-module.exports = con.model('User', userSchema);
+module.exports = con.model('Date', dateSchema);
