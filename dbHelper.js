9348d2f19dad02d14495d6e645c6061ebc3eaceb
Erkam Seker <serkamseker@gmail.com>
2017-03-25 06:41:09 +0300

    getUsers changed

--- dbHelper.js
+++ dbHelper.js
@@ -2,7 +2,37 @@ var userDb = require("./models/User");
 var dateDb = require("./models/Hangout");
 var mongoose = require('mongoose');
 
-var getUsers = function(place, date, hour, callback){
+var getUsers = function(id, place, date, hour, callback){
+	newHangout(id, place, date, hour);
+	
+	userDb.find({ id: {'$ne': id}, hangout_place: place, hangout_date: date, hangout_hour: hour }, function(err, users){
+		if(err){
+			console.log(err);
+			throw err;
+		} 
+		callback(users);
+	});
+
+}
+
+var newHangout = function(id, place, date, hour){
+	userDb.findOne({ id: id }, function(err, user){
+		if(err){
+			console.log(err);
+			throw err;
+		} 
+		user.hangout_place = place;
+		user.hangout_date = date;
+		user.hangout_hour = hour;
+
+		user.save(function(err) {
+  			if (err) console.log(err);
+  				console.log(user.first_name + ' updated successfully!');
+		});
+	});
+
+}
+var getHangouts = function(place, date, hour, callback){
 	userDb.find({ hangout_place: place, hangout_date: date, hangout_hour: hour }, function(err, users){
 		if(err){
 			console.log(err);
--- index.js
+++ index.js
@@ -4,14 +4,33 @@ var dbHelper = require('./dbHelper');
 var app = express();
 
 app.get('/getUsers', function (req, res) {
+	id = req.query.id;
 	place = req.query.place;
 	date = req.query.date;
-	hour = req.query.hour;
-	dbHelper.getUsers(place, date, hour, function(users){
+	hour = req.squery.hour;
+	dbHelper.getUsers(id, place, date, hour, function(users){
 		console.log(JSON.stringify(users));
 		res.send(JSON.stringify(users));
 	});
 });
+
+app.route('/newHangout').post(function(req, res) {
+    id = req.query.id;
+ 	place = req.query.place;
+ 	date = req.query.date;
+	hour = req.query.hour;
+	dbHelper.newHangout(id, place, date, hour);
+});
+
+/*
+app.get('/newHangout', function (req, res) {
+	id = req.query.id;
+	place = req.query.place;
+	date = req.query.date;
+	hour = req.query.hour;
+	dbHelper.newHangout(id, place, date, hour);
+});
+*/
 
 var server = app.listen(process.env.PORT || 8082, function () {
    var host = server.address().address;
